c6be829c658bf19fde962286ad75c121d5a2e7ec
Gizem Urgancı <gurganci@k1m32s07.42istanbul.com.tr>
2025-09-01 18:06:02 +0300

    complete project

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gurganci <gurganci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/01 14:40:17 by gurganci          #+#    #+#             */
+/*   Updated: 2025/09/01 15:59:09 by gurganci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "PhoneBook.hpp"
 
 
@@ -11,17 +23,16 @@ int main()
     while (1) 
     {
         std::cout << "Enter command (ADD, SEARCH, EXIT): ";
-        std::cin >> line;
-        
-        if (line == "ADD")
-            phoneBook.add();
-        else if (line == "SEARCH")
-            phoneBook.search();
-        else if (line == "EXIT")
-        {
-            
+        std::getline(std::cin, line);
+
+        if(std::cin.eof())
             break;
-        }
+        if (line == "ADD" || line == "Add" || line == "add")
+            phoneBook.add();
+        else if (line == "SEARCH" || line == "Search" || line == "search")
+            phoneBook.search();
+        else if (line == "EXIT" || line == "exit" || line == "Exit")
+            break;
         else
             std::cout << "Invalid command! Use: ADD, SEARCH, EXIT" << std::endl;
     }
